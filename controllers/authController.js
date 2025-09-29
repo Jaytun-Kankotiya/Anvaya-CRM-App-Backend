a705cd55f1b10d0509c633dcd86c02b065029c8e
Jaytun Kankotiya <jaytunkankotiya81@gmail.com>
2025-09-29 19:43:38 -0400

    Updated Version

--- controllers/authController.js
+++ controllers/authController.js
@@ -30,7 +30,7 @@ export const register = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production", 
-      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax", 
+      sameSite: 'none',
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
@@ -76,8 +76,8 @@ export const login = async (req, res) => {
     });
     res.cookie("token", token, {
       httpOnly: true,
-      secure: false,
-      sameSite: "lax",
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: 'none',
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
